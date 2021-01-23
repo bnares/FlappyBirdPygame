2681cc0abeb1bd3c39e17b74ff5744f2177163ca
Piotr Ostrouch <piotr.ostrouch@gmail.com>
2021-01-23 22:38:28 +0100

    basic movment

--- FlappyBird.py
+++ FlappyBird.py
@@ -1,8 +1,29 @@
 import pygame, sys
 
 pygame.init()
-screen = pygame.display.set_mode((500,680))
+
+width = 280
+height = 510
+screen = pygame.display.set_mode((width,height))
+
 clock = pygame.time.Clock()
+
+
+bgSurface = pygame.image.load("background-day.png").convert()
+floorSurface = pygame.image.load("base.png").convert()
+
+#bgSurface = pygame.transform.scale2x(bgSurface)
+
+#increment values
+
+floorXPos = 0
+
+
+def drawFloor():
+    screen.blit(floorSurface, (floorXPos, height-70))
+    screen.blit(floorSurface, (floorXPos+width, height-70))
+
+
 
 while True:
 
@@ -11,4 +32,8 @@ while True:
             pygame.quit()
             sys.exit()
 
+    screen.blit(bgSurface, (0,0))
+    drawFloor()
+    floorXPos -=1
     clock.tick(60)
+    pygame.display.update()
