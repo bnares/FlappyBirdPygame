89434f67f717f399e47c7878027fcdffc75affbd
Piotr Ostrouch <piotr.ostrouch@gmail.com>
2021-01-23 23:01:51 +0100

    display bird

--- FlappyBird.py
+++ FlappyBird.py
@@ -11,6 +11,8 @@ clock = pygame.time.Clock()
 
 bgSurface = pygame.image.load("background-day.png").convert()
 floorSurface = pygame.image.load("base.png").convert()
+bird = pygame.image.load("bluebird-midflap.png").convert()
+birdPos = bird.get_rect(center = (width/2, height/2))
 
 #bgSurface = pygame.transform.scale2x(bgSurface)
 
@@ -22,6 +24,8 @@ floorXPos = 0
 def drawFloor():
     screen.blit(floorSurface, (floorXPos, height-50))
     screen.blit(floorSurface, (floorXPos+width, height-50))
+
+
 
 
 
@@ -37,5 +41,7 @@ while True:
     floorXPos -=1
     if floorXPos <-width:
         floorXPos =0
+
+    screen.blit(bird, birdPos)
     clock.tick(60)
     pygame.display.update()
